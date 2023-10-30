02731a540908bb5f2ea67e6fe638af58932bd1f4
Justin <menziesjustin@ymail.com>
2023-10-30 18:37:14 -0400

    - added leap year calculator

---
+++ IsLeapYear.java
@@ -0,0 +1,15 @@
+public class IsLeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1600));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
