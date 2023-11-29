7387fb6faa897ff163d255e6cbebbaab08948a6c
Justin <menziesjustin@ymail.com>
2023-11-29 03:19:52 -0500

    - reformatted

--- MegaBytesConverter.java
+++ MegaBytesConverter.java
@@ -1,19 +1,19 @@
 public class MegaBytesConverter {
-    public static void main(String[] args) {
-        printMegaBytesAndKiloBytes(2500);
-    }
+  public static void main(String[] args) {
+    printMegaBytesAndKiloBytes(2500);
+  }
 
-    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
-        if (kiloBytes < 0) {
-            System.out.println("Invalid Value");
-        } else {
-            int originalKiloBytes = kiloBytes;
-            int megaBytes = 0;
-            while (kiloBytes >= 1000) {
-                kiloBytes -= 1000;
-                megaBytes++;
-            }
-            System.out.println(originalKiloBytes + " KB = " + megaBytes + " MB and " + kiloBytes + " KB");
-        }
+  public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+    if (kiloBytes < 0) {
+      System.out.println("Invalid Value");
+    } else {
+      int originalKiloBytes = kiloBytes;
+      int megaBytes = 0;
+      while (kiloBytes >= 1000) {
+        kiloBytes -= 1000;
+        megaBytes++;
+      }
+      System.out.println(originalKiloBytes + " KB = " + megaBytes + " MB and " + kiloBytes + " KB");
     }
+  }
 }
--- SpeedConverter.java
+++ SpeedConverter.java
@@ -1,22 +1,23 @@
 public class SpeedConverter {
-    public static void main(String[] args) {
-        speedConversion(115);
-    }
+  public static void main(String[] args) {
+    speedConversion(115);
+  }
 
-    public static long toMilesPerHour(double kilometersPerHour) {
-        if (kilometersPerHour < 0) {
-            return (-1);
-        } else {
-            long mph = Math.round(kilometersPerHour * .621371);
-            return mph;
-        }
+  public static long toMilesPerHour(double kilometersPerHour) {
+    if (kilometersPerHour < 0) {
+      return (-1);
+    } else {
+      long mph = Math.round(kilometersPerHour * .621371);
+      return mph;
     }
+  }
 
-    public static void speedConversion(double kilometersPerHour) {
-        if (kilometersPerHour < 0) {
-            System.out.println("Invalid input");
-        } else {
-            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
-        }
+  public static void speedConversion(double kilometersPerHour) {
+    if (kilometersPerHour < 0) {
+      System.out.println("Invalid input");
+    } else {
+      System.out
+          .println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
     }
+  }
 }
