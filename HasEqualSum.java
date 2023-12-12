7850e30ac61df028c5906f4eebab0b24e1da3f57
Justin <menziesjustin@ymail.com>
2023-12-11 20:08:20 -0500

    changed comment spacing

--- HasEqualSum.java
+++ HasEqualSum.java
@@ -1,13 +1,14 @@
 public class HasEqualSum {
-  public static void main(String[] args) {
-    System.out.println(hasEqualSum(1, -1, 0));
-  }
 
-  public static boolean hasEqualSum(int num1, int num2, int num3) {
-    if (num1 + num2 == num3) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+	public static void main(String[] args) {
+		System.out.println(hasEqualSum(1, -1, 0));
+	}
+
+	public static boolean hasEqualSum(int num1, int num2, int num3) {
+		if (num1 + num2 == num3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
